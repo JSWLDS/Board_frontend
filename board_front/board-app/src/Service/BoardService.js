94972399a2d1a08ef99003f6e446df13ecb7da78
JSW <jswlds2@gmail.com>
2023-11-13 16:43:27 +0900

    proxy

--- board_front/board-app/src/Service/BoardService.js
+++ board_front/board-app/src/Service/BoardService.js
@@ -1,12 +1,11 @@
 import axios from 'axios'; 
 
 
-const API_BASE_URL ="http://localhost:8080/api";
-const BOARD_API_BASE_URL = "http://localhost:8080/api/board"; 
+const API_BASE_URL ="http://localhost:8080/api/v1";
+const BOARD_API_BASE_URL = "http://localhost:8080/api/v1/board"; 
 
 class BoardService {
-
-    
+     
 
     getAllBoards() {
         return axios.get(BOARD_API_BASE_URL);
@@ -15,7 +14,7 @@ class BoardService {
     createBoard(board) {
         return axios.post(BOARD_API_BASE_URL, board);
     }
-
+    
     updateCount(boardId){
         return axios.patch(BOARD_API_BASE_URL + '/' + boardId);
     }
