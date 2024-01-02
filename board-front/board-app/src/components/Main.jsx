69a3d1415e92134bc178b97765d49722cd57e1ac
JSW <jswlds2@gmail.com>
2024-01-02 16:38:42 +0900

    main 페이지 중앙.

--- board-front/board-app/src/components/Main.jsx
+++ board-front/board-app/src/components/Main.jsx
@@ -4,8 +4,10 @@ import React from 'react';
 function Main() {
   
     return (
-        <div>
-            하이요. 메인 페이지 입니다<div className=""></div>
+        <div className='main'>
+
+            하이요. 메인 페이지 입니다.
+            <div className="main_img"></div>
         </div>
     );
 }
